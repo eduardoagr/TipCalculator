2482769d4825b705a48dd0005758ec0fc895595a
eduardoagr <egomezr@outlook.com>
2021-01-19 19:04:47 +0100

    finished

---
+++ TipCalculator/TipCalculator/TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace TipCalculator {
+    public partial class MainPage : ContentPage {
+        public MainPage() {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Clicked(object sender, EventArgs e) {
+
+            var total = decimal.Parse(entCellTotal.Text);
+            var tip = decimal.Parse(entCellPorcent.Text);
+            var NumberOfPeople = decimal.Parse(entCellNumberOfPeople.Text);
+
+            var TotalTip = ( total * tip)/100;
+
+            TxtTip.Detail = TotalTip.ToString();
+            TxtTotalBill.Detail = (TotalTip + total).ToString("C");
+            TxtTotaTipPerPerson.Detail = (TotalTip / NumberOfPeople).ToString("C");
+            TxtTotalPerson.Detail = ((total + TotalTip) / NumberOfPeople).ToString("C");
+
+
+        }
+    }
+}
